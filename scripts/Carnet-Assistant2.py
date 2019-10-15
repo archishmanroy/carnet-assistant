9da41abc9b3189e8f5d36df4d75ff1866e8f4fb5
Archishman Roy <archishmanroy@Archishmans-MacBook-Pro.local>
2019-10-15 19:58:35 +0200

    initial commit

---
+++ scripts/Carnet-Assistant2.py
@@ -0,0 +1,275 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess, gspread, sys, json, pdfrw, argparse
+from os import path, system, mkdir, listdir
+from natsort import natsorted
+from operator import itemgetter, attrgetter
+from oauth2client.service_account import ServiceAccountCredentials
+from time import strftime
+from utils2 import *
+
+
+def setupArgumentParser():
+    parser = argparse.ArgumentParser(
+        description='provide name of google worksheet (not filename!)',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-b',
+        '--base',
+        help='base directory (e.g. ~/Carnet-Assistant)',
+        type=str,
+        required=True)
+    parser.add_argument(
+        '-s',
+        '--sheet',
+        help='Name of worksheet',
+        type=str,
+        required=True)
+    parser.add_argument(
+        '-t',
+        '--test',
+        help='Use file names to label images, not json dict',
+        type=bool,
+        required=False,
+        default=0)
+    parser.add_argument(
+        '-f',
+        '--font',
+        help='Path of some system font eg Ubuntu-B.ttf',
+        type=str,
+        default='/usr/share/fonts/truetype/ubuntu-font-family/Gravity-Bold.otf')
+    parser.add_argument(
+        '-k',
+        '--credentialsAccount',
+        help='Spreadsheet service account key credentials file',
+        type=str,
+        default='service-account-key.json')
+    parser.add_argument(
+        '--credentialsSheet',
+        help='Spreadsheet key file',
+        type=str,
+        default='spreadsheet.key')
+    return parser
+
+
+###############################
+############ MAIN #############
+###############################
+
+if __name__ == "__main__":
+    options = setupArgumentParser().parse_args()
+    basedir = options.base
+    path_images = path.join(basedir, "images", options.sheet)
+    path_fdf_dir = path.join(basedir, "dicts")
+    path_pdf_templates = path.join(basedir, "templates")
+    carnet_output_dir = path.join(basedir, "output")
+    page_output_dir = path.join(basedir, "output/.pages")
+
+    archive = strftime("%Y%m%d%H%M%S") + "-" + options.sheet
+    carnet_archive_dir = path.join(basedir, "output/archive", archive)
+
+    extra_templates = ["extra-front.pdf", "extra-back.pdf"]
+    front_page_templates = ["antrag-blank.pdf", "carnet-blank.pdf"]
+    outputs = ["antrag.pdf", "carnet.pdf", "images.pdf"]
+
+    ##############################################
+    # use creds to create client for G-Drive API #
+    ##############################################
+    creds_account = path.join(basedir, "creds/", options.credentialsAccount)
+    creds_sheet = path.join(basedir, "creds/", options.credentialsSheet)
+    scope = ['https://www.googleapis.com/auth/drive', 'https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name(creds_account, scope)
+    client = gspread.authorize(creds)
+
+    #####################################
+    # Find workbook  and open worksheet #
+    #####################################
+    with open(creds_sheet, 'r') as file:
+        spreadsheet_key = file.read().replace('\n', '')
+    sheet = client.open_by_key(spreadsheet_key).worksheet(options.sheet)
+
+    #############################################################################
+    # Parse entries as list of dictionaries. There will be ten entries per page #
+    #############################################################################
+    data = sheet.get_all_records()
+
+    if checkCountries(data):
+        print('Exiting.')
+        quit()
+
+    if checkImages(data, path_images):
+        print('Exiting.')
+        quit()
+
+    nentries = len(data)
+    npages = nentries // 10
+    nlastpage = nentries % 10  # number of list entries on the last page. used to fill the final dictionary with some empty values to avoid null pointer
+    print("pages: {0}, items on last page: {1}".format(npages, nlastpage))
+
+    #########################################
+    # Write dictionary for numbering images #
+    #########################################
+    dict_images = {"archive": archive}
+    for item in range(nentries):
+        key_image = data[item]['image']
+        value_item = data[item]['item']
+        dict_images[key_image] = value_item
+
+    with open("{}/image_dict.json".format(path_fdf_dir), "w") as file:
+        file.write(json.dumps(dict_images))
+
+    ##################################
+    # Empty old pages from directory #
+    ##################################
+    for current_files in listdir(page_output_dir):
+        file_path = path.join(page_output_dir, current_files)
+        try:
+            if path.isfile(file_path):
+                if current_files != '.gitignore':
+                    unlink(file_path)
+        except Exception as e:
+            print(e)
+
+    ###########################################
+    # Create FDF files from the list of dicts #
+    # Define header and footer and keys       #
+    ###########################################
+    fdf_header = "%FDF-1.2\n1 0 obj<</FDF<< /Fields[\n\n"
+    extra_footer = "<< /T({W.C})/V({}) >>\n<< /T({W.E})/V({}) >>\n"
+    fdf_footer = "\n] >> >>\nendobj\ntrailer\n<</Root 1 0 R>>\n%%EOF"
+    data_keys = {"item": "A", "description": "B", "quantity": "C", "mass": "D", "value": "E", "country": "F",
+                 "image": "Z"}
+    unit = {"C": "", "D": " kg", "E": " EUR"}
+    page_sums = [{"C": 0, "D": 0, "E": 0}]
+    for counter in range(0, npages + 1):
+        page_sums.append({"C": 0, "D": 0, "E": 0})
+
+    ###################
+    # Start page loop #
+    ###################
+    for page_number in range(npages + 1):
+        first_item = page_number * 10
+        last_item = (page_number + 1) * 10
+
+        ########################################
+        # Open a new fdf file and write header #
+        ########################################
+        with open("{0}/page-{1}.fdf".format(path_fdf_dir, page_number), "w+") as file:
+            file.write(fdf_header)
+            # Write the initial P sums
+            carried_sums = "\n"
+            print(page_sums)
+            for key in page_sums[page_number]:
+                carried_sums += "<< /T(P.{0})/V({1}{2}) >>\n".format(key, page_sums[page_number][key] / 100, unit[key])
+                if page_number == 0:
+                    file.write("<< /T(P.{0})/V({1}) >>\n".format(key, ""))
+                elif page_number in range(1, npages + 1):
+                    file.write(carried_sums)
+                    print('carried_sums= ' + carried_sums)
+                page_sums[page_number + 1]["C"] = page_sums[page_number]["C"]
+                page_sums[page_number + 1]["D"] = page_sums[page_number]["D"]
+                page_sums[page_number + 1]["E"] = page_sums[page_number]["E"]
+
+            ############################
+            # Loop over next ten items #
+            ############################
+            for item in range(first_item, last_item):
+                if item == len(data):
+                    for empty in range(item % 10, 10):
+                        for key in data[0]:
+                            if key == "image":
+                                continue
+                            key_string = "{0}.{1}".format(empty, data_keys[key])
+                            empty_key_value_pair = "<< /T({0})/V({1}) >>\n".format(key_string, "")
+                            file.write(empty_key_value_pair)
+                    break
+
+                ############################
+                # Loop over each parameter #
+                ############################
+                for key in data[item]:
+                    form_field = data_keys[key]
+                    if form_field == "Z" or "":
+                        continue
+
+                    #######################################
+                    # Write the <<\T()\V()>> pair to file #
+                    #######################################
+                    key_string = "{0}.{1}".format(item % 10, form_field)
+                    value_string = str(data[item][key])
+                    key_value_pair = "<< /T({0})/V({1}) >>\n".format(key_string, value_string)
+                    file.write(key_value_pair)
+
+                    ###############
+                    # Update sums #
+                    ###############
+                    if form_field == "C":
+                        page_sums[page_number + 1]["C"] += int(data[item][key] * 100)
+                    if form_field == "D":
+                        page_sums[page_number + 1]["D"] += int(data[item][key] * 100)
+                    if form_field == "E":
+                        page_sums[page_number + 1]["E"] += int(data[item][key] * 100)
+                    # print(page_sums[page_number+1]["D"])
+
+            #########################
+            # Write page final sums #
+            #########################
+            for key in page_sums[page_number + 1]:
+                print('page sums')
+                print(page_sums)
+                print('key= ' + key)
+                if page_number == npages + 1:
+                    break
+                sums_key_value_pair = "<< /T(S.{0})/V({1}{2}) >>\n".format(key,
+                                                                           float(page_sums[page_number + 1][key]) / 100,
+                                                                           unit[key])
+                print(str(page_number) + '\t' + str(item) + '\t' + sums_key_value_pair)
+                file.write(sums_key_value_pair)
+                # print(sums_key_value_pair)
+
+            ###################################
+            # Write footer and close fdf file #
+            ###################################
+            file.write(extra_footer)
+            file.write(fdf_footer)
+            parsed_fdf = file.read()
+
+        #############################################################
+        # bash command pdftk uses fdf to create, fill and write pdf #
+        #############################################################
+        fill_form_in_bash = "pdftk {0}/{1} fill_form {2}/page-{4}.fdf output {3}/page-{4}.pdf flatten".format(
+            path_pdf_templates, extra_templates[page_number % 2], path_fdf_dir, page_output_dir, page_number)
+        system(fill_form_in_bash)
+
+    ##############################
+    # Assemble the finished list #
+    ##############################
+    finished_list_pages = []
+    for finished_list_page in natsorted(listdir(page_output_dir)):
+        finished_list_pages.append("{0}/{1}".format(page_output_dir, str(finished_list_page)))
+    str_finished_list = " ".join(finished_list_pages)
+
+    ###########################
+    # Assemble the final pdfs #
+    ###########################
+    yeeah = (" ( •_•)", "( •_•)>⌐■-■\n(⌐■ _■ )")
+    for index in range(len(front_page_templates)):
+        current_front_page = path.join(path_pdf_templates, front_page_templates[index])
+        system("pdftk {0} {1} cat output {2}".format(current_front_page, str_finished_list,
+                                                     path.join(carnet_output_dir, outputs[index])))
+        print("{} \t File written {}".format(yeeah[index], outputs[index]))
+
+    ################################
+    # Create image pdf if possible #
+    ################################
+    assembleImages(basedir, options.sheet, outputs[2], carnet_archive_dir, options.font, options.test)
+
+    ##################
+    # Archive a copy #
+    ##################
+    mkdir(carnet_archive_dir)
+    system("cp {} {}".format(path.join(carnet_output_dir, outputs[0]), carnet_archive_dir))
+    system("cp {} {}".format(path.join(carnet_output_dir, outputs[1]), carnet_archive_dir))
+    system("cp {} {}".format(path.join(carnet_output_dir, outputs[2]), carnet_archive_dir))
+    print("\n\n (\____/) \n( ͡ ͡° ͜ ʖ ͡ ͡°)\n  \╭☞ \╭☞\n\n \t ʕ •́؈•̀)\nArchived in {}.".format(archive))
---
+++ scripts/utils2.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+from PIL import Image, ImageDraw, ImageFont
+from os import path, listdir, getcwd, unlink, system
+from natsort import natsorted
+import shutil, json, argparse
+from time import strftime
+
+
+def checkImages(data, containingDirectory):
+    # Flags missing images, extra images, and duplicate filenames
+    images = []
+    for element in data:
+        images.append(str(element['image']))
+
+    files = listdir(containingDirectory)
+    filelist = [path.splitext(path.basename(filename))[0] for filename in files]
+    missing_dir = [filename for filename in filelist if filename not in images]
+    missing_list = [element for element in images if element not in filelist]
+    duplicates_set = set([x for x in filelist if filelist.count(x) > 1])
+    if len(duplicates_set) > 0 or len(data) != len(files) or len(missing_list) > 0:
+        print('Image check failed.')
+        if len(duplicates_set) > 0:
+            print(
+                        'ERROR Filenames must be unique. Dont forget to update spreadsheet if you change one. \n\tDuplicated files: ' + ', '.join(
+                    duplicates_set))
+        print(len(data))
+        print(len(files))
+        if (len(data) < len(files)):
+            print("ERROR Did you take too many selfies?")
+            print("\tThe spreedshet lacks {} image/s.".format(abs(len(data) - len(listdir(containingDirectory)))))
+            print("\tImages missing from the spreadsheet: {}".format(', '.join(missing_dir)))
+
+        if (len(data) > len(files)):
+            print("ERROR Did you put too many entries?")
+            print("\tThe image folder lacks {} image/s.".format(abs(len(data) - len(listdir(containingDirectory)))))
+        if len(missing_list) > 0:
+            print('ERROR Filenames do not match up:')
+            print("\tImages missing from the spreadsheet: {}".format(', '.join(missing_dir)))
+            print("\tImages missing from the folder: {}".format(', '.join(missing_list)))
+        return 1
+
+    return 0
+
+
+def checkCountries(data):
+    # Check Country names are correct #
+
+    country_codes = ['AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ',
+                     'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BV', 'BR',
+                     'IO', 'BN', 'BG', 'BF', 'BI', 'CV', 'KH', 'CM', 'CA', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC',
+                     'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CW', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO',
+                     'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF', 'GA',
+                     'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY', 'HT',
+                     'HM', 'VA', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM', 'JP',
+                     'JE', 'JO', 'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI',
+                     'LT', 'LU', 'MO', 'MK', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX',
+                     'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'NC', 'NZ', 'NI',
+                     'NE', 'NG', 'NU', 'NF', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN',
+                     'PL', 'PT', 'PR', 'QA', 'RE', 'RO', 'RU', 'RW', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS',
+                     'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SX', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS', 'SS',
+                     'ES', 'LK', 'SD', 'SR', 'SJ', 'SZ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK',
+                     'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ', 'VU',
+                     'VE', 'VN', 'VG', 'VI', 'WF', 'EH', 'YE', 'ZM', 'ZW']
+
+    entries = []
+    for element in data:
+        entries.append(str(element['country']))
+    wrong_code = [entry for entry in entries if entry not in country_codes]
+    if len(wrong_code) > 0:
+        print('Country check failed.')
+        print("\nUh oh- is that country on mars?: {}".format(wrong_code))
+        return 1
+    else:
+        return 0
+
+
+def assembleImages(basedir, sheet, out, archive_dir, font, labeltype):
+    #######################
+    # Define output paths #
+    #######################
+    output_dir = path.join(basedir, "output")
+    preprocessed_images_dir = path.join(basedir, "images", sheet)
+    postprocessed_images_dir = path.join(basedir, "images/.post")
+    assembled_pages_dir = path.join(basedir, "images/.pages")
+    image_dict = path.join(basedir, "dicts/image_dict.json")
+    outfile = path.join(output_dir, out)
+    # archive_dir = path.join(output_dir,'archive',images_dict['archive'])
+
+    if sheet not in listdir(path.join(basedir, "images")):
+        print("Sheet name doesnt exist as image directory. Exiting.")
+        quit()
+
+    #########################################
+    # Empty intermediate processing folders #
+    #########################################
+
+    for current_files in listdir(postprocessed_images_dir):
+        file_path = path.join(postprocessed_images_dir, current_files)
+        try:
+            if path.isfile(file_path):
+                if current_files != '.gitignore':
+                    unlink(file_path)
+        except Exception as e:
+            print(e)
+
+    for current_files in listdir(assembled_pages_dir):
+        file_path = path.join(assembled_pages_dir, current_files)
+        try:
+            if path.isfile(file_path):
+                if current_files != '.gitignore':
+                    unlink(file_path)
+        except Exception as e:
+            print(e)
+
+    ########################################
+    # Define Page size and image placement #
+    ########################################
+    n_w, n_h = 2, 4
+    dpi = 300
+    w, h = int(8.27 * dpi), int(11.69 * dpi)  # A4 dimension 2481 * 3507
+
+    header, footer, lmargin, rmargin = 100, 100, 100, 100
+    r_hgap, r_vgap = 0.1, 0.2
+
+    image_w, image_h = int((w - (lmargin + rmargin)) / (n_w + (n_w - 1) * r_hgap)), int(
+        (h - (footer + header)) / (n_h + (n_h - 1) * r_vgap))
+    hgap, vgap = int((w - (lmargin + rmargin) - n_w * image_w) / (n_w - 1)), int(
+        (h - (footer + header) - n_h * image_h) / (n_h - 1))
+
+    if image_w * n_w + hgap * (n_w - 1) + lmargin + rmargin > w or image_h * n_h + vgap * (
+            n_h - 1) + footer + header > h:
+        print("Computation error!")
+
+    ##########
+    # Images #
+    ##########
+    size = (image_w, image_h)
+    overlay_x0 = 0
+    overlay_y0 = 0
+    overlay_height = image_h / 10
+    overlay_width = image_w / 6
+    overlay_x1 = overlay_x0 + overlay_width
+    overlay_y1 = overlay_y0 + overlay_height
+    overlay_size = int(overlay_height * 0.8)
+
+    with open(image_dict) as file:
+        images_dict = json.load(file)
+
+    ##################
+    # Process images #
+    ##################
+    for img_infile_name in natsorted(listdir(preprocessed_images_dir)):
+        if img_infile_name.startswith('.'):
+            continue
+        # get the file name and location
+        img_infile = path.join(preprocessed_images_dir, img_infile_name)
+        # get the image number from the dictionary
+        if labeltype == 1:
+            overlay_text = path.splitext(img_infile_name)[0]
+        elif labeltype == 0:
+            overlay_key = path.splitext(img_infile_name)[0]
+            overlay_text = str(images_dict[overlay_key])
+
+        img_outfile = path.join(postprocessed_images_dir, "_{0}.jpeg".format(overlay_text))
+
+        # resize the image and convert to RGBA
+        img = Image.open(img_infile).convert('RGB')
+        img.thumbnail(size)
+
+        # add the text box
+        draw = ImageDraw.Draw(img)
+        draw.rectangle(((overlay_x0, overlay_y0), (overlay_x1, overlay_y1)), fill="white")
+        fnt = ImageFont.truetype(font, overlay_size)
+        draw.text((overlay_x0, overlay_y0), overlay_text, font=fnt, fill=(0, 0, 0, 255))
+        del draw
+
+        # save the output
+        img.save(img_outfile, "JPEG")
+        print("Image {0} saved".format(img_infile_name))
+
+    ##################
+    # Assemble pages #
+    ##################
+    n_pages = int(float(len(listdir(postprocessed_images_dir))) / (n_w * n_h))
+    for n_page in range(n_pages + 1):
+        page = Image.new('RGB', (w, h), 'white')
+        for file in range(n_w * n_h):
+            # print(file)
+            # print(n_page * n_h * n_w + file)
+            if not path.isfile(path.join(postprocessed_images_dir, "_{}.jpeg".format(n_page * n_h * n_w + file + 1))):
+                break
+            pos = (int(lmargin + ((file * image_w + (file - file / n_w) * hgap) % (w - (lmargin + rmargin)))),
+                   header + file // n_w * (image_h + vgap))
+            # print(", ".join(file, header, file, n_w, image_h, vgap))
+            page.paste(
+                Image.open(path.join(postprocessed_images_dir, "_{}.jpeg".format(n_page * n_h * n_w + file + 1))),
+                box=pos)
+        page.save(path.join(assembled_pages_dir, "page_{0:02d}.pdf".format(n_page)))
+
+    #################################
+    # Get list of all created pages #
+    #################################
+    list_of_pages = []
+    for element in natsorted(listdir(assembled_pages_dir)):
+        list_of_pages.append(path.join(assembled_pages_dir, str(element)))
+    str_list_of_pages = " ".join(list_of_pages)
+
+    ##########################
+    # Assemble the final pdf #
+    ##########################
+    files = sorted([path.join(assembled_pages_dir, file) for file in listdir(assembled_pages_dir)])
+    file_list = " ".join(files)
+    system("pdftk {0} cat output {1}".format(file_list, outfile))
+
+    # How to use the IMAGE ASSISTANT ################################################################
+    #												#
+    # 1. All images in /pre/ will be resized and graphically labeled with a number			#
+    # 2. Two choices of how to label, use test=0 for json dictionary or 1 for image filename	#
+    # 3. Processes images saved in /post/								#
+    # 4. Then assembled into A4 pages (pdfs) and saved in pages					#
+    # 5. Finally the pages are concatenated. Output pdf saved in ../Output/				#
+    #												#
+    # BY CATRIONA BRUCE 15/6/2018									#
+    # catriona.bruce@tum.de										#
+    # To create an the image list for WH3 carnet							#
+    #												#
+    #												#
+    # New users should define output paths, make sure their directory structure matches: 		#
+    # Carnet-wh3(Carnet-Assistant(credentials,dicts,templates),Images(pages,pre,post),Output)	#
+    #################################################################################################
+
+    #########################################################################################
+    # Test mode is for when the spreadsheet doesn't match the pictures in source directory, #
+    # In test mode (1) the pictures are labeled by file name,				#
+    # otherwise (0) to use the dictionary created by carnet assistant    			#
+    #########################################################################################
+
